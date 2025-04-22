f4e8a4731446323758441540a8bc54473600d0ae
Sergi Camps <sergi.camps@adsmurai.com>
2025-04-22 16:48:24 +0200

    bypass cache control by using the headers blacklist

--- src/apicache.js
+++ src/apicache.js
@@ -249,7 +249,8 @@ function ApiCache() {
 
     var headers = getSafeHeaders(response)
 
-    Object.assign(headers, filterBlacklistedHeaders(cacheObject.headers || {}), {
+    Object.assign([...headers, {
+      // SCT This is my modified version, so we can bypass this header too by using the blacklist
       // set properly-decremented max-age header.  This ensures that max-age is in sync with the cache expiration.
       'cache-control':
         'max-age=' +
@@ -257,7 +258,7 @@ function ApiCache() {
           0,
           (duration / 1000 - (new Date().getTime() / 1000 - cacheObject.timestamp)).toFixed(0)
         ),
-    })
+    }], filterBlacklistedHeaders(cacheObject.headers || {}))
 
     // only embed apicache headers when not in production environment
     if (process.env.NODE_ENV !== 'production') {
